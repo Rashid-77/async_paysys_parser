919d41ba79b0fea51d3b5c5f75f36471e3c35dd1
Rashid <rash.sh.77@gmail.com>
2023-04-08 23:28:44 +0500

    unistream_pay added, tested

--- parser_a.py
+++ parser_a.py
@@ -6,6 +6,7 @@ from src.app.exch_parser.binance import get_binance_p2p
 from src.app.exch_parser.calc_spreads import calculate_korona_binance_spread_new
 from src.app.config.parse_countries import get_country_list
 from src.app.exch_parser.korona_pay import parse_korona
+from src.app.exch_parser.unistream_pay import parse_unistream
 
 from src.logger import get_logger
 
@@ -20,6 +21,7 @@ db = SpreadDb("spreads")
 exchrate_binance = []
 exch_bin_rdy = False
 exchrate_korona_pay = []
+exchrate_unistream_pay = []
 
 
 async def parse_bin():
@@ -91,11 +93,50 @@ async def parse_korpay():
         await asyncio.sleep(rest_time)
 
 
+async def parse_unistr():
+    global exch_bin_rdy
+    logger.info('--> "parse_unistr()"')
+    countries = get_country_list('Unistream')
+
+    while True:
+        start_t = time.time()
+        try:
+            logger.debug('"unistr" start parse_unistr()')
+            await parse_unistream(countries, exchrate_unistream_pay)
+
+            logger.debug('"unistr"  wait exch_bin_rdy')
+            while not exch_bin_rdy:
+                await asyncio.sleep(1)
+            spreads_new = calculate_korona_binance_spread_new(
+                                    exchrate_binance, exchrate_unistream_pay)
+            logger.debug(f'{spreads_new=}')
+            logger.debug('"unistr" calc finished')
+            await db.write_spread_v2(spreads_new)
+            logger.debug('"unistr" written  spread_v2')
+
+            # for i in await db.read_allspread_v2():
+            #     logger.info(f"  {i}")
+
+        except Exception as e:
+            logger.error(e)
+
+        end_t = time.time()
+        logger.info(f'- unistr { (end_t - start_t):.3f} sec -')
+        rest_time = PARSE_TIME_PERIOD_SEC - (end_t - start_t)
+        if rest_time <= 0:
+            logger.debug(f'- unistr longer by {(-rest_time):.1f} s')
+            continue
+        if rest_time > PARSE_TIME_PERIOD_SEC:
+            rest_time = PARSE_TIME_PERIOD_SEC
+        await asyncio.sleep(rest_time)
+
+
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     tasks = [
         loop.create_task(parse_bin()), 
         loop.create_task(parse_korpay()), 
+        loop.create_task(parse_unistr()), 
     ]
     wait_tasks = asyncio.wait(tasks)
     loop.run_until_complete(wait_tasks)
---
+++ src/app/exch_parser/unistream_pay.py
@@ -0,0 +1,93 @@
+import asyncio
+import os
+from src.app.exch_parser.req_check_err_a import request_get_json
+from src.app.exch_parser.country_fiat_info import fiat_info
+
+import traceback
+from inspect import currentframe, getframeinfo
+from src.logger import get_logger
+
+logger = get_logger('unistr', 'parser', 'unistr')
+logger.info("\nProgram started")
+
+TOKEN_PATH = '/code/src/app/exch_parser/unistr_token.txt'
+
+unistream_dirs = {
+    'pay_name':'Unistream',
+    'Турция':[{'country_code':'TUR', 'currency':'TRY','amount':30000}],
+    'Узбекистан':[  
+                    {'country_code':'UZB', 'currency':'USD', 'amount':800},
+                    {'country_code':'UZB', 'currency':'EUR', 'amount':800},
+                    {'country_code':'UZB', 'currency':'UZS', 'amount':800000},
+                ],
+}
+
+
+async def get_unistream_exch_rates(country_name:str, alter_way:dict, token:str) -> tuple:
+    country_code = alter_way['country_code']
+    currency = alter_way['currency']
+    amount = alter_way['amount']
+    url = f'https://online.unistream.ru/api/card2cash/calculate?' \
+            f'destination={country_code}&amount={amount}&currency={currency}&' \
+            f'accepted_currency=RUB&profile=unistream_front'
+
+    header = {
+        'User-Agent': "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:108.0) "\
+                        "Gecko/20100101 Firefox/108.0",
+        "authorization": token
+    }    
+
+    json_d = await request_get_json(url, header)
+    
+    fiat = currency
+    if fiat == 'USD' or fiat == 'EUR':
+        fiat += fiat_info.get_country_region_code(country_name)
+
+    pay_name = unistream_dirs['pay_name']
+    if json_d is None:
+        d = (f'{pay_name}-{country_name}-RUB-{fiat}', 'proto_err', 0, 0, 0)
+        logger.error('json is None (protocol error)')
+        clear_token()
+        return d
+    
+    try:
+        sendingAmount = json_d['fees'][0]['acceptedAmount']
+        withdrawAmount = json_d['fees'][0]['withdrawAmount']
+        acceptedTotalFee = json_d['fees'][0]['acceptedTotalFee']
+        exch_rate = sendingAmount / withdrawAmount
+        exch_rate = round(exch_rate, 5) if fiat == 'UZS' else  round(exch_rate, 2)
+        d = (f'{pay_name}-{country_name}-{fiat}', str(exch_rate), str(acceptedTotalFee), 0, 0)
+    except Exception as e:
+        frameinfo = getframeinfo(currentframe())
+        logger.error(f'{traceback.format_exc()} {frameinfo.filename} {frameinfo.lineno} \n{e}')
+        d = (f'{pay_name}-{country_name}-{fiat}', 'no_price', 0, 0, 0)
+        clear_token()
+    return d
+
+
+async def parse_unistream(countries:tuple, data_list:list):
+    token = check_token()
+    logger.info('----')
+    if token == '':
+        logger.info('token not exist - wait for token')
+        return
+
+    for country in countries:
+        for way in unistream_dirs[country]:
+            d = await get_unistream_exch_rates(country, way, token)
+            data_list.append(d)
+            logger.info(d)
+            await asyncio.sleep(1)
+
+
+def check_token() -> str:
+    token = ''
+    if os.path.exists(TOKEN_PATH):
+        with open(TOKEN_PATH, 'r') as f:
+            token = f.read()
+    return token
+
+
+def clear_token():
+    with open(TOKEN_PATH, 'w') as f:
+        token = f.write('None')
